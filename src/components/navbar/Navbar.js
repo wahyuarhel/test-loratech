e974a99f69724980a759c8bfeccd0338c39b49e0
Wahyu Ramadhan <akunwahyuarhel@gmail.com>
2021-03-18 21:55:17 +0700

    Initial commit

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Navbar.scss'
+
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <h3>TESt</h3>
+        </div>
+    )
+}
