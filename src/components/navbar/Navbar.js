a482d8776149dc6ab87f784472196268db10a8b7
Wahyu Ramadhan <akunwahyuarhel@gmail.com>
2021-03-21 02:34:34 +0700

    add scroll to top when click menu

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -12,23 +12,31 @@ export default function Navbar() {
 
     window.addEventListener('scroll', shadowEffect)
 
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+    }
     return (
         <div className={scroll ? 'navbar active' : 'navbar'}>
             <div className="navbar_container">
                 <div>
-                    <Link to="/"><img src="https://static.toss.im/icons/svg/logo-every-moment-white.svg" alt="logo" /></Link>
+                    <Link to="/" onClick={scrollToTop}>
+                        <img src="https://static.toss.im/icons/svg/logo-every-moment-white.svg" alt="logo" />
+                    </Link>
                 </div>
                 <div>
-                    <Link to='/'>
+                    <Link to='/' onClick={scrollToTop}>
                         <div>의견 남기기</div>
                     </Link>
-                    <Link to='/menu_2'>
+                    <Link to='/menu_2' onClick={scrollToTop}>
                         <div>TOP 20</div>
                     </Link>
-                    <Link to='/menu_3'>
+                    <Link to='/menu_3' onClick={scrollToTop}>
                         <div>실제 개선 사례</div>
                     </Link>
-                    <Link to='/menu_4'>
+                    <Link to='/menu_4' onClick={scrollToTop}>
                         <div>캠페인 자세히 보기</div>
                     </Link>
                 </div>
